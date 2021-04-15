d3316c55505e75f6b637d9939bb4d18efda43460
Kevin <kevinhuynh914@gmail.com>
2021-04-15 00:31:04 -0700

    yes

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import re
 from urllib.parse import urlparse
 from lxml.html.soupparser import fromstring
 from lxml.etree import tostring
+from lxml import etree
 
 ics_urls = [".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu", ".stat.uci.edu", "today.uci.edu/department/information_computer_sciences/"]
 
@@ -14,18 +15,44 @@ def extract_next_links(url, resp):
     # Implementation required.
 
     result = []
-    ##TODO: checking HTML
-    html = resp.raw_response.content
-    root = fromstring(html)
-    html_string = tostring(root).strip().decode("utf-8") 
+    try:
+
+        if resp.raw_response:
+            html = resp.raw_response.content
+            root = fromstring(html)
+            html_string = tostring(root).strip().decode("utf-8") 
 
 
-    hrefs = re.findall(r'href=".+" ', html_string)
+            hrefs = re.findall(r'href=".+" ', html_string)
 
-    for href in hrefs:
-        # print(h.split()[0])
-        # parsed = urlparse(h.split()[0].strip("href=").strip('"'))
-        result.append(href.split()[0].strip("href=").strip('"'))
+            for href in hrefs:
+                # print(h.split()[0])
+                # parsed = urlparse(h.split()[0].strip("href=").strip('"'))
+                result.append(href.split()[0].strip("href=").strip('"'))
+
+    except Exception as e:
+        print(e)
+        print()
+
+        try: 
+            if resp.raw_response:
+                html = resp.raw_response.content
+                root = etree.fromstring(html)
+                html_string = etree.tostring(root).strip().decode("utf-8") 
+
+
+                hrefs = re.findall(r'href=".+" ', html_string)
+
+                for href in hrefs:
+                    # print(h.split()[0])
+                    # parsed = urlparse(h.split()[0].strip("href=").strip('"'))
+                    result.append(href.split()[0].strip("href=").strip('"'))
+
+        except Exception as e:
+            print(e)
+            if "Opening and ending tag mismatch" in str(e):
+                return []
+            
 
 
     return result
@@ -35,17 +62,21 @@ def is_valid(url):
 
     try:
         parsed = urlparse(url)
+
         if parsed.scheme not in set(["http", "https"]):
             return False
 
         is_ics_url = False
 
-        is_ics_url =  any(url in parsed.geturl() for url in ics_urls)
+        correct_ics_url = parsed.scheme + parsed.netloc + parsed.path
+        is_ics_url =  any(url in correct_ics_url for url in ics_urls)
 
         if not is_ics_url:
             return False 
 
-        
+        if re.match(r".*respond.*|.*reply.*|.*comment.* ",parsed.query) or re.match(r".*pdf.*|.*respond.*|.*reply.*|.*comment.* ",parsed.params):
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
