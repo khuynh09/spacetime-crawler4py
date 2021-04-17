b233355d9118abcfda7852788e363d49b1a16606
jadonk42 <jadonkwan2@gmail.com>
2021-04-16 23:33:39 -0700

    changed tokenize()

--- crawler/worker.py
+++ crawler/worker.py
@@ -6,6 +6,7 @@ from scraper import scraper
 import time
 from bs4 import BeautifulSoup
 from utils import tokenizer
+import re
 
 # .ics.uci.edu
 # 
@@ -20,17 +21,51 @@ class Worker(Thread):
         self.word_freq = {}
         self.longest_page = [] # [page url, wordCount]
         self.subdomains = {}
+        self.stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't",
+                "as", "at", "be", "because", "been",
+                "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
+                "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for",
+                "from", "further", "had", "hadn't", "has", "hasn't",
+                "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+                "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll",
+                "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's",
+                "me", "more", "most", "mustn't", "my",
+                "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our",
+                "ours", "ourselves", "out",
+                "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't",
+                "so", "some", "such", "than", "that", "that's", "the", "their",
+                "theirs", "them", "themselves", "then", "there",
+                "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to",
+                "too",
+                "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were",
+                "weren't", "what", "what's", "when", "when's", "where", "where's", "which",
+                "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you",
+                "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+
         super().__init__(daemon=True)
 
 
-    # def tokenize(content):
-    #     for line in openedFile:
-    #             tokens = re.sub(r'[^\w\s]|_', '', line).rstrip(
-    #                 '\r\n').rstrip().split(" ")
-    #             i = 0
-    #             while i < len(tokens):
-    #                 tokens[i] = tokens[i].lower()
-    #                 i += 1
+    def tokenize(self, content):
+        token_list = []
+        for line in content.split():
+            tokens = re.sub(r'[^\w\s]|_', '', line).rstrip(
+                '\r\n').rstrip().split(" ")
+            i = 0
+            while i < len(tokens):
+                tokens[i] = tokens[i].lower()
+                i += 1
+            token_list.extend(tokens)
+            
+
+        for token in token_list:
+            if (token in self.word_freq):
+                self.word_freq[token] += 1
+            else:
+                self.word_freq[token] = 1
+        
+
+
+
     def run(self):
 
         while True:
@@ -42,7 +77,7 @@ class Worker(Thread):
 
             if 200 <= resp.status < 300 and resp.raw_response:
                 soup  = BeautifulSoup(resp.raw_response.content, features="lxml")
-                print(soup.get_text(separator=" "))
+                self.tokenize(soup.get_text(separator=" "))
                 self.logger.info(
                     f"Downloaded {tbd_url}, status <{resp.status}>, "
                     f"using cache {self.config.cache_server}.")
@@ -61,3 +96,6 @@ class Worker(Thread):
                         
                 self.frontier.mark_url_complete(tbd_url)
                 time.sleep(self.config.time_delay)
+
+            print(sorted(self.word_freq.items(), key=lambda x: x[1], reverse=True)[0:10])
+        
